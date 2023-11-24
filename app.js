6f388ac88b7b3687321add4b629396b77db9d36c
vishwahegdek <vishwahegdek@gmail.com>
2023-11-24 13:13:13 +0530

    Styles.css connected to all templates

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const port = 3000
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const entryRouter = require('./routes/entry.js');
 
